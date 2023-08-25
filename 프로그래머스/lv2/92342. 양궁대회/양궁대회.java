db27323fd72199537e90d13974fe2f5c8654aa03
Damongsanga <110401199+Damongsanga@users.noreply.github.com>
2023-08-25 11:31:30 +0900

    [level 2] Title: 양궁대회, Time: 0.56 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -10,36 +10,44 @@ class Solution {
 	}
 	
 	public int[] solution(int n, int[] info) {
-		dfs(0, n, new int[11], info);
+		backtracking(0, n, new int[11], info);
 		return answersum == 0 ? new int[] { -1 } : answer;
 	}
 
-	static void dfs(int depth, int n, int[] ryan, int[] info) {
+	
+	static void backtracking(int depth, int n, int[] ryan, int[] info) {
 		
+		// 깊이가 10이면 남은 화살을 모두 0으로 넣고 점수 계산
 		if (depth == 10 || n == 0) {
 			ryan[10] = n;
 			int diff = diff(ryan, info);
 			if (diff > answersum) {
 				answer = Arrays.copyOf(ryan, ryan.length);
 				answersum = diff;
+			// 점수차이가 기존 답과 같으면 더 낮은 화살 수가 많은지 체크
 			} else if (diff == answersum) {
 				if (mincheck(ryan))
 					answer = Arrays.copyOf(ryan, ryan.length);
 			}
+			// 원복 필요!
 			ryan[10] = 0;
 			return;
 		}
 
-		dfs(depth + 1, n, ryan, info);
+		// 화살을 못맞추고 넘어가는 경우 먼저 고려
+		backtracking(depth + 1, n, ryan, info);
+		// 어피치보다 많이 할 수 잇는 경우
 		if (n > info[depth]) {
 			ryan[depth] = info[depth] + 1;
-			dfs(depth + 1, n - info[depth] - 1, ryan, info);
+			backtracking(depth + 1, n - info[depth] - 1, ryan, info);
 			ryan[depth] = 0;
+		// 불가능한 경우 바로 depth 10으로 이동 (성능 개선)
 		} else {
-			dfs(10, n, ryan, info);
+			backtracking(10, n, ryan, info);
 		}
 	}
 
+	// 두 선수의 점수 차이 
 	static int diff(int[] ryan, int[] info) {
 		int diff = 0;
 		for (int i = 0; i <= 10; i++) {
@@ -52,7 +60,7 @@ class Solution {
 		return diff;
 	}
 	
-	
+	// 전의 정답과 비교하여 더 낮은 점수의 화살이 더 많은지 체크
 	static boolean mincheck(int[] ryan) {
 		for (int i = 10; i >= 0; i--) {
 			if (answer[i] > ryan[i]) return false;
