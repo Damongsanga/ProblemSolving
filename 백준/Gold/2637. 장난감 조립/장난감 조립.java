faafd5b3d53644d8ed5ce4a3d85f20eb439ff1f1
Damongsanga <110401199+Damongsanga@users.noreply.github.com>
2023-09-19 22:50:09 +0900

    [Gold II] Title: 장난감 조립, Time: 80 ms, Memory: 11692 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.java
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.java
@@ -1,25 +1,17 @@
 import java.io.*;
 import java.util.*;
 
-class Node {
-    int r;
-    int c;
-
-    Node(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-}
-
 public class Main {
-    static int[] rr = {-1, 0, 1, 0};
-    static int[] rc = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
+        // 장난감 조립 정보를 저장할 인접리스트
         ArrayList<Integer[]>[] arr = new ArrayList[N+1];
+        int[] inV = new int[N+1];
+        int[] answer = new int[N+1];
+
         for (int i = 1; i <= N; i++) {
             arr[i] = new ArrayList<>();
         }
@@ -30,45 +22,28 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             arr[a].add(new Integer[] {b, c});
+            inV[b]++;
         }
 
-        int[] answer = new int[N];
-
-        // pq는 높은 레벨 순서대로
-        PriorityQueue<Integer[]> pq = new PriorityQueue<>(((o1, o2) -> o2[0] - o1[0]));
-        pq.offer(new Integer[] {N,1});
-        int level;
-        int count = 0;
-        while (!pq.isEmpty()){
-            Integer[] now = pq.poll();
-            level = now[0];
-            count += now[1];
-            // 모아서 아래 레벨 넣기
-            if (!pq.isEmpty() && now[0] == pq.peek()[0]){
-                continue;
-            } else {
-                if (arr[level].size() == 0) {
-                    answer[level] += count;
-                } else {
-                    for (Integer[] tmp: arr[level]) {
-                        pq.offer(new Integer[] {tmp[0], tmp[1] * count});
-                    }
-                }
-                count = 0;
+        Queue<Integer> queue = new ArrayDeque();
+        queue.add(N);
+        answer[N] = 1;
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            for (Integer[] tmp : arr[now]){
+                int toy = tmp[0];
+                int num = tmp[1];
+                answer[toy] += answer[now] * num;
+                if(--inV[toy] == 0) queue.add(toy);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < N; i++) {
-            if (answer[i] > 0){
+        for (int i = 1; i <= N; i++) {
+            if (arr[i].size() == 0)
                 sb.append(i + " " + answer[i] + "\n");
-            }
         }
-
         System.out.println(sb);
 
-
     }
-
-
 }
