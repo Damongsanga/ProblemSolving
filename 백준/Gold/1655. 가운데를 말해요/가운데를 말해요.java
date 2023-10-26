9746e4fb68525dbe50ed55a62b6c189d0f00e56c
Damongsanga <110401199+Damongsanga@users.noreply.github.com>
2023-10-26 15:36:17 +0900

    [Gold II] Title: 가운데를 말해요, Time: 412 ms, Memory: 47824 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -2,20 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static final long MOD = 1_000_000_007;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		// 작은 값들을 담을 maxpq
 		PriorityQueue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
+		// 큰 값들을 담을 minpq
 		PriorityQueue<Integer> minpq = new PriorityQueue<>();
 		
 		StringBuilder sb = new StringBuilder();
-		
-		// 초기화 1번째
+		// 첫번째 값 넣기
 		maxpq.add(Integer.parseInt(br.readLine()));
 		sb.append(maxpq.peek() + "\n");		
 		
+		// 지금까지 쌓인 숫자 갯수가 홀수냐 짝수냐로 분기
+		// 짝수일 경우 앞쪽의 수를 구하라고 했기 때문에 maxpq에서 peek 한 값을 출력해야
 		for (int i = 1; i < N; i++) {
 			int n = Integer.parseInt(br.readLine());
 			if (maxpq.size() == minpq.size()) {
