290c2a0db8f755ea403ea305e65e93d27aa77c2e
Damongsanga <110401199+Damongsanga@users.noreply.github.com>
2023-11-10 20:42:41 +0900

    [level 4] Title: 호텔 방 배정, Time: 636.98 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -4,6 +4,7 @@ class Solution {
     public long[] solution(long k, long[] room_number) {
         int N = room_number.length;
         long[] answer = new long[N];
+        // key - value 를 유니온 파인드의 파인드 알고리즘의 자식 - 부모로 생각해보자
         Map<Long, Long> map = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
@@ -13,10 +14,12 @@ class Solution {
         return answer;
     }
     static long DFS(long a, Map<Long, Long> map){
+        // 한번도 방문한 적이 없으면 다음 방을 자신의 부모로 설정
         if (!map.containsKey(a)){
             map.put(a, a+1);
             return a;
         }
+        // 부모가 지정되지 않을 때까지 부모로 탐색하면서 갱신 (path shortening)
         map.put(a,DFS(map.get(a), map));
         return map.get(a);
     }
